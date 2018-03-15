b9d64a0825b780f083a06643f494a1ae86c892a1
Olmo Gallegos <olmo@appandweb.es>
2018-03-15 12:46:08 +0100

    Initial commit

---
+++ app/src/test/kotlin/es/voghdev/playbattlegrounds/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package es.voghdev.playbattlegrounds
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    @Throws(Exception::class)
+    fun `addition is correct`() = assertEquals(4L, (2L + 2L))
+}
