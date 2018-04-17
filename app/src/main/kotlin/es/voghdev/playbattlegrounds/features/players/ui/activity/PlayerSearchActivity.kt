80b5ae3b779cba2ce72f3f8f69e94e32aa183253
Olmo Gallegos <olmo@appandweb.es>
2018-04-17 08:04:23 +0200

    Wire up a little bit

--- app/src/main/kotlin/es/voghdev/playbattlegrounds/features/players/ui/activity/PlayerSearchActivity.kt
+++ app/src/main/kotlin/es/voghdev/playbattlegrounds/features/players/ui/activity/PlayerSearchActivity.kt
@@ -1,9 +1,12 @@
 package es.voghdev.playbattlegrounds.features.players.ui.activity
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import com.appandweb.peep.ui.activity.BaseActivity
 import es.voghdev.playbattlegrounds.R
 import es.voghdev.playbattlegrounds.common.reslocator.AndroidResLocator
+import es.voghdev.playbattlegrounds.common.ui.ColoredSnackbar
+import es.voghdev.playbattlegrounds.features.players.api.request.GetPlayerByNameApiDataSource
 import es.voghdev.playbattlegrounds.features.players.ui.presenter.PlayerSearchPresenter
 import kotlinx.android.synthetic.main.activity_player_search.*
 import kotlinx.coroutines.experimental.runBlocking
@@ -14,7 +17,7 @@ class PlayerSearchActivity : BaseActivity(), PlayerSearchPresenter.MVPView, Play
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        presenter = PlayerSearchPresenter(AndroidResLocator(this))
+        presenter = PlayerSearchPresenter(AndroidResLocator(this), GetPlayerByNameApiDataSource())
         presenter?.view = this
         presenter?.navigator = this
 
@@ -23,11 +26,21 @@ class PlayerSearchActivity : BaseActivity(), PlayerSearchPresenter.MVPView, Play
         }
 
         rootView.setOnClickListener {
-            presenter.onSendButtonClicked("some_player")
+            presenter?.onSendButtonClicked("some_player")
         }
     }
 
     override fun getLayoutId(): Int {
         return R.layout.activity_player_search
+    }
+
+    override fun showPlayerName(name: String) {
+        tv_player_name.text = name
+    }
+
+    override fun showError(message: String) {
+        val snackbar = Snackbar.make(rootView, message, Snackbar.LENGTH_LONG)
+
+        ColoredSnackbar.alertBold(snackbar).show()
     }
 }
