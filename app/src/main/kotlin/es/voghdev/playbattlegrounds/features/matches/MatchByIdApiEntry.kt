2bf579b1f492bf03113c769de2f6b54e4c6caafb
Olmo Gallegos <olmo@appandweb.es>
2018-05-02 11:50:32 +0200

    Remove old api classes

--- app/src/main/kotlin/es/voghdev/playbattlegrounds/features/matches/MatchByIdApiEntry.kt
+++ app/src/main/kotlin/es/voghdev/playbattlegrounds/features/matches/MatchByIdApiEntry.kt
@@ -1,11 +1,9 @@
 package es.voghdev.playbattlegrounds.features.matches
 
-import es.voghdev.playbattlegrounds.features.matchesv1.ParticipantApiEntry
-
 class MatchByIdApiEntry(
         val type: String,
         val id: String,
         val attributes: MatchAttributesApiEntry?,
         val relationships: MatchRelationshipsApiEntry,
-        val included: List<ParticipantApiEntry>
+        val included: List<ParticipantByIdApiEntry>
 )
