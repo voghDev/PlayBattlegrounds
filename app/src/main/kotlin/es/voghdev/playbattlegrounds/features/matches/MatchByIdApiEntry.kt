c172fd2f9fcc5064acba46af356dcef4cd090fdc
Olmo Gallegos <olmo@appandweb.es>
2018-05-02 11:48:38 +0200

    Plenty more JSON entities

--- app/src/main/kotlin/es/voghdev/playbattlegrounds/features/matches/MatchByIdApiEntry.kt
+++ app/src/main/kotlin/es/voghdev/playbattlegrounds/features/matches/MatchByIdApiEntry.kt
@@ -1,6 +1,11 @@
 package es.voghdev.playbattlegrounds.features.matches
 
+import es.voghdev.playbattlegrounds.features.matchesv1.ParticipantApiEntry
+
 class MatchByIdApiEntry(
         val type: String,
-        val id: String
+        val id: String,
+        val attributes: MatchAttributesApiEntry?,
+        val relationships: MatchRelationshipsApiEntry,
+        val included: List<ParticipantApiEntry>
 )
