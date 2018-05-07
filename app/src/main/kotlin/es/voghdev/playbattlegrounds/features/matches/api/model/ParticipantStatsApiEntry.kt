e3927c9eb8890e3b858b8ba827cae8716f0348dd
Olmo Gallegos <olmo@appandweb.es>
2018-05-07 07:56:10 +0200

    Rest of the fields that API contains

--- app/src/main/kotlin/es/voghdev/playbattlegrounds/features/matches/api/model/ParticipantStatsApiEntry.kt
+++ app/src/main/kotlin/es/voghdev/playbattlegrounds/features/matches/api/model/ParticipantStatsApiEntry.kt
@@ -16,5 +16,33 @@
 package es.voghdev.playbattlegrounds.features.matches.api.model
 
 class ParticipantStatsApiEntry(
-        val DBNOs: Int
+        val DBNOs: Int?,
+        val assists: Int?,
+        val boosts: Int?,
+        val damageDealt: Float?,
+        val deathType: String?,
+        val headshotKills: Int?,
+        val heals: Int?,
+        val killPlace: Int?,
+        val killPoints: Int?,
+        val killPointsDelta: Float?,
+        val killStreaks: Int?,
+        val kills: Int?,
+        val lastKillPoints: Int?,
+        val lastWinPoints: Int?,
+        val longestKill: Int?,
+        val mostDamage: Int?,
+        val name: String?,
+        val playerId: String?,
+        val revives: Int?,
+        val rideDistance: Int?,
+        val roadKills: Int?,
+        val teamKills: Int?,
+        val timeSurvived: Int?,
+        val vehicleDestroys: Int?,
+        val walkDistance: Float?,
+        val weaponsAcquired: Int?,
+        val winPlace: Int?,
+        val winPoints: Int?,
+        val winPointsDelta: Float?
 )
